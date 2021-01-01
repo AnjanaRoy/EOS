34122283bc9b6ac79418f7e645f8f7b5475d77ea
Anjana Roy <anjanaroy2598@gmail.com>
2021-01-02 02:11:48 +0545

    Buff IO demo

---
+++ syscall/buffio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello\n"); //buffer
+    printf("world\n");
+    return 0;
+}
+
+// no of \n - write 1
+// \n - write2
